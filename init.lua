925dd6f4f6d65d6a25b45218c9769a72b72bceec
RollieGarcia0031 <garciarollie827@gmail.com>
2025-10-02 22:17:16 +0800

    fix telescope

--- init.lua
+++ init.lua
@@ -47,6 +47,11 @@ require("lazy").setup({
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
       local builtin = require("telescope.builtin")
+      require("telescope").setup({
+        defaults = {
+          file_ignore_patterns = { "node_modules/", "vendor/", "%.class" },
+        },
+      })
       vim.keymap.set("n", "<leader>f", builtin.find_files, { desc = "Find files" })
       vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Live grep" })
       vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Find buffers" })
